bc98ad53a9ce20cca28d718c23a0576e904f9eb8
Murad Kamali <murad.coding@gmail.com>
2023-08-08 13:43:41 +0100

    delete unnecessary files

--- src/utils/convertMapToArray.ts
+++
@@ -1,7 +0,0 @@
-import { Player, PlayerData } from "@/types/socketio";
-
-export const convertMapToArray = (players: Map<string, Player>): PlayerData[] =>
-  Array.from(players, ([key, player]) => ({
-    id: key,
-    player,
-  }));
--- src/utils/handleTimeout.ts
+++
@@ -1,7 +0,0 @@
-import { toast } from "react-toastify";
-
-export const handleTimeout = () => {
-  toast.error("Timeout: refresh the page to reconnect to server", {
-    autoClose: false,
-  });
-};
