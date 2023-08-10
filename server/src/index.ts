84db3a6e411a5db8327d1294003374253924ddda
Murad Kamali <murad.coding@gmail.com>
2023-08-10 15:04:00 +0100

    removes unused package from package.json

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,35 +3,15 @@ import { createServer } from "http";
 import { Server as SocketIOServer } from "socket.io";
 import { instrument } from "@socket.io/admin-ui";
 import { setupSocketEvents } from "./controllers/socketEvents.js";
-import cors from "cors";
-import 'dotenv/config'
 
 const app = express();
-const testApp = express();
 const port = 8080;
 
 const allowedOrigins = [
   "http://localhost:3000",
   "https://admin.socket.io",
-  "https://multidoku.vercel.app", // don't really need this nginx will reverse proxy to socketio server
+  "https://multidoku.vercel.app",
 ];
-
-if (process.env.SERVER_ADDRESS) {
-  allowedOrigins.push(process.env.SERVER_ADDRESS);
-}
-
-testApp.use(
-  cors({
-    origin: allowedOrigins,
-    credentials: true,
-  })
-);
-
-testApp.listen(8081, () => {
-  console.log("API running on 8081");
-});
-
-testApp.get("/", (req, res) => res.json("Connected to server"));
 
 const server = createServer(app);
 const io = new SocketIOServer(server, {
